f23dc0411d1dbd4cb6436bd2f69f1cb8524af589
michele.scarlato@gmail.com <michele.scarlato@gmail.com>
2020-11-21 21:58:56 +0900

    HTTP server simple version

---
+++ server.py
@@ -0,0 +1,10 @@
+#https://www.afternerd.com/blog/python-http-server/
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
