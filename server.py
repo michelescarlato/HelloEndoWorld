a092964d87a82628d80fc6e8e69c7f19d098b630
michele.scarlato@gmail.com <michele.scarlato@gmail.com>
2020-11-21 22:43:31 +0900

    python3 server.py -p 8081 or default 8080
    Set error message if port number is not between 0 and 65535

--- server.py
+++ server.py
@@ -3,11 +3,30 @@ import http.server
 import socketserver
 import logging
 import sys
+import argparse
 
-try:
-    PORT = int(sys.argv[1])
-except:
-    PORT = 8080
+class PortAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if not 0 < values < 2**16:
+            raise argparse.ArgumentError(self, "port numbers must be between 0 and 65535")
+        setattr(namespace, self.dest, values)
+
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument("-p", "--port",
+                        help='Port number to connect to',
+                        dest='port',
+                        default=8080,
+                        type=int,
+                        action=PortAction,
+                        metavar="{0..65535}")
+
+args = parser.parse_args()
+if args.port:
+    #print("port set"+str(args.port))
+    PORT = args.port
 
 Handler = http.server.SimpleHTTPRequestHandler
 
