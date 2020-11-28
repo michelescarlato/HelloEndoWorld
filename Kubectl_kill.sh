8fee7b607afa2b83a1403b8a258db54a12ed18ce
Michele Scarlato <michele.scarlato@gmail.com>
2020-11-29 01:11:53 +0900

    Kubernetes file and running and killing scripts updated.

--- Kubectl_kill.sh
+++ Kubectl_kill.sh
@@ -1,2 +1,2 @@
 sudo kubectl delete service helloendoworld-service && sleep 1
-sudo kubectl delete deployment helloendoworld && sleep 1
+sudo kubectl delete job helloendoworld-job && sleep 1
--- kubectl_run-job.sh
+++ kubectl_run-job.sh
@@ -1,10 +1,10 @@
 sudo kubectl apply -f helloendoworld-kubernetes-job.yaml && sleep 1
 sudo kubectl get job helloendoworld-job && sleep 1
 sudo kubectl describe job helloendoworld-job && sleep 1
-sudo kubectl get replicasets && sleep 1
-sudo kubectl describe replicasets && sleep 1
-sudo kubectl expose job helloendoworld-job --type=NodePort --name=helloendoworld-service && sleep 1
+#sudo kubectl get replicasets && sleep 1
+#sudo kubectl describe replicasets && sleep 1
+
 # take the NodePort number
-sudo kubectl describe services helloendoworld-service && sleep 1
-sudo kubectl get pods --selector="run=load-balancer-example" --output=wide && sleep 1
-sudo kubectl describe services helloendoworld-service | grep NodePort && sleep 1
+#sudo kubectl describe services helloendoworld-service && sleep 1
+sudo kubectl get pods --selector="run=helloendoworld-batch" --output=wide && sleep 1
+#sudo kubectl describe services helloendoworld-service | grep NodePort && sleep 1
