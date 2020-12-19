fd26aaf29bafd264daf380332ea56f4e6afbcabd
Michele Scarlato <michele.scarlato@gmail.com>
2020-12-19 23:44:52 +0900

    trying to understand why namespace is still creating

--- helm_commands.sh
+++ helm_commands.sh
@@ -13,9 +13,9 @@ sudo kubectl describe pod
 sudo kubectl get pod
 
 echo "Waiting $TIMER seconds"
-sleep $TIMER
+#sleep $TIMER
 echo "$TIMER seconds passed"
-sudo kubectl port-forward $POD_NAME 8080:8080 > port_forwarding.log &
+#sudo kubectl port-forward $POD_NAME 8080:8080 > port_forwarding.log &
 sleep 5
 
 
