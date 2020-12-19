efc2f9ea45b29ae9fd99c3475d677400c39113b0
Michele Scarlato <michele.scarlato@gmail.com>
2020-12-19 22:05:51 +0900

    Timer parameterized

--- helm_commands.sh
+++ helm_commands.sh
@@ -1,3 +1,5 @@
+TIMER=60
+
 sudo helm install helloendoworld-chart helloendoworld-chart/ --values helloendoworld-chart/values.yaml
 export NODE_PORT=$(sudo kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services helloendoworld-chart)
 export NODE_IP=$(sudo kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
@@ -8,7 +10,10 @@ export POD_NAME=$(sudo kubectl get pods -l "app.kubernetes.io/name=helloendoworl
 sudo kubectl describe pod
 
 sudo kubectl get pod
-sleep 60
+
+sleep $TIMER
+echo "Waiting $TIMER seconds"
+
 sudo kubectl port-forward $POD_NAME 8080:8080
 #curl http://121.179.158.56:8080/helloworld
 #sudo helm uninstall helloendoworld-chart
